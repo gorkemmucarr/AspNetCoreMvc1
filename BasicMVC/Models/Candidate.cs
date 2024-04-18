adeab147319149b2b279ef59f8342a388c4d97e2
Görkem Uçar <117277191+gorkemmucarr@users.noreply.github.com>
2024-04-18 17:21:42 +0300

    Add project files.

---
+++ BasicMVC/Models/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicMVC.Models
+{
+    public class Candidate
+    {
+        [Required (ErrorMessage ="Email cant be empty")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "FirstName cant be empty")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "LastName cant be empty")]
+        public string? LastName { get; set; }
+        public string? FullName => $"{FirstName} {LastName}";
+        public int? Age { get; set; }
+        public DateTime ApplyAt { get; set; }
+        public string? SelectedCourse { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;   
+        }
+    }
+    
+}
